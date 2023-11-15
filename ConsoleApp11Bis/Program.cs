d9a6c714946b12edfd653c612b54045dea5ac95b
Ali MAKRI <github@makri.fr>
2023-11-15 15:08:03 +0100

    AM

---
+++ ConsoleApp11Bis/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11Bis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Version 1
+            Dictionary<string, Vol> vols = new Dictionary<string, Vol>();
+            vols.Add("Sétif", new Vol { Origine = "Lyon", Destination = "Sétif", Horaire = new DateTime(2023, 11, 22, 6, 0, 0) });
+            vols.Add("Singapour", new Vol { Origine = "Lyon", Destination = "Singapour", Horaire = new DateTime(2023, 12, 5, 6, 0, 0) });
+            vols.Add("Tanger", new Vol { Origine = "Lyon", Destination = "Tanger", Horaire = new DateTime(2023, 12, 8, 6, 0, 0) });
+
+            // Demande de destination
+            Console.Write("Destination : ");
+            var destination = Console.ReadLine();
+
+            // Résultat - Version 1
+            var vol = vols[destination];
+            Console.WriteLine("Un vol est prévu le {0} pour aller à {1}", vol.Horaire, vol.Destination);
+
+            Console.ReadLine();
+        }
+    }
+    class Vol
+    {
+        public string Origine;
+        public string Destination;
+        public string Numero;
+        public DateTime Horaire;
+    }
+}
--- Exo_ConsoleApp11/Program.cs
+++ Exo_ConsoleApp11/Program.cs
@@ -10,7 +10,38 @@ namespace Exo_ConsoleApp11
     {
         static void Main(string[] args)
         {
+            // Version 1
+            //List<Vol> vols = new List<Vol>();
+            //vols.Add(new Vol { Origine = "Lyon", Destination = "Sétif", Horaire = new DateTime(2023, 11, 22, 6, 0, 0) });
+            //vols.Add(new Vol { Origine = "Lyon", Destination = "Singapour", Horaire = new DateTime(2023, 12, 5, 6, 0, 0) });
+            //vols.Add(new Vol { Origine = "Lyon", Destination = "Tanger", Horaire = new DateTime(2023, 12, 8, 6, 0, 0) });
 
+            // Version 2
+            List<Vol> vols = new List<Vol>
+            {
+                new Vol { Origine = "Lyon", Destination = "Sétif", Horaire = new DateTime(2023, 11, 22, 6, 0, 0) },
+                new Vol { Origine = "Lyon", Destination = "Singapour", Horaire = new DateTime(2023, 12, 5, 6, 0, 0) },
+                new Vol { Origine = "Lyon", Destination = "Tanger", Horaire = new DateTime(2023, 12, 8, 6, 0, 0) }
+            };
+
+            // Demande de destination
+            Console.Write("Destination : ");
+            var destination = Console.ReadLine();
+
+            // Résultat - Version 1
+            //foreach (var vol in vols)
+            //{
+            //    if (vol.Destination.ToUpper() == destination.ToUpper())
+            //    {
+            //        Console.WriteLine("Un vol est prévu le {0} pour aller à {1}", vol.Horaire, vol.Destination);
+            //    }
+            //}
+
+            // Résultat - Version Linq
+            var volTrouve = vols.First(vol => vol.Destination.ToUpper() == destination.ToUpper());
+            Console.WriteLine("Un vol est prévu le {0} pour aller à {1}", volTrouve.Horaire, volTrouve.Destination);
+
+            Console.ReadLine();
         }
     }
     class Vol
