206ce3be577d59a87b0c837241390468f8c08b12
Ali MAKRI <github@makri.fr>
2023-11-15 11:58:44 +0100

    AM

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -12,9 +12,25 @@ namespace ConsoleApp7
         {
             int cavLigne = 1;
             int cavColonne = 1;
-            int[] deplaceLigne = new int[8] { 1, -1, 1, -1, 2, -2, 2, -2 };
+            int[] deplaceLigne = new int[8]  { 1, -1, 1, -1, 2, -2, 2, -2 };
             int[] deplaceColonne = new int[8] { 2, 2, -2, -2, 1, 1, -1, -1 };
+            Random rand = new Random();
 
+            int index = 0;
+            while (cavLigne != 8 || cavColonne != 8)
+            {
+                int alea = rand.Next(0, 8);
+                int ligne = cavLigne + deplaceLigne[alea];
+                int colonne = cavColonne + deplaceColonne[alea];
+                if (ligne > 0 && colonne > 0 && ligne < 9 && colonne < 9)
+                {
+                    index++;
+                    cavLigne = ligne;
+                    cavColonne = colonne;
+                    Console.WriteLine("{2}. {0}{1}", (char) (cavColonne+64), cavLigne, index);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var elephant1 = new Elephant { Nom = "Babar" };
+            Elephant.Nombre++;
+
+            Elephant elephant2 = new Elephant();
+            elephant1.Nom = "Dumbo";
+            Elephant.Nombre++;
+
+            Cheval cheval1 = new Cheval();
+            cheval1.Nom = "Jolly Jumper";
+            Console.WriteLine("le cheval s'appelle {0} et l'éléphant s'appelle {1}",
+                cheval1.Nom, elephant1.Nom);
+            Console.WriteLine("Il y a {0} éléphant(s)", Elephant.Nombre);
+            cheval1.Courir();
+
+            Animal x = null;
+            Animal cheval2 = new Cheval { Nom = "Petit tonnerrre" };
+            Animal elephant3 = new Elephant { Nom = "Heraclus" };
+            var saisie = Console.ReadLine();
+            if (saisie.ToUpper() == "C")
+                x = cheval2;
+            else
+                x = elephant3;
+            x.Courir();
+
+            Console.Read();
+        }
+    }
+    class Cheval : Animal
+    {
+        public override void Courir()
+        {
+            Console.WriteLine("ça galope !");
+        }
+    }
+    class Elephant : Animal
+    {
+        public static int Nombre = 0;
+        public override void Courir()
+        {
+            Console.WriteLine("ça piétine !");
+        }
+    }
+    class Animal
+    {
+        public string Nom;
+        public virtual void Courir()
+        {
+        }
+    }
+}
